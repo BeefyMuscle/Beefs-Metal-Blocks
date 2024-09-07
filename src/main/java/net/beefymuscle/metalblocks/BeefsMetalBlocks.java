478d2d1cb8b98ef43e11fff72d7d2efa6201bc35
BeefyMuscle <BenjaminCM.Boyes@gmail.com>
2024-09-07 03:32:34 -0500

    Initial commit using fabric mod template

---
+++ src/main/java/net/beefymuscle/metalblocks/BeefsMetalBlocks.java
@@ -0,0 +1,16 @@
+package net.beefymuscle.metalblocks;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BeefsMetalBlocks implements ModInitializer {
+	public static final String MOD_ID = "beefsmetalblock";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/beefymuscle/metalblocks/BeefsMetalBlocksClient.java
@@ -0,0 +1,10 @@
+package net.beefymuscle.metalblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeefsMetalBlocksClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
