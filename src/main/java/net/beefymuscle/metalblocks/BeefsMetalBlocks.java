6d1ac0b1a7e223c829f40ca5308dc369edc006d8
BeefyMuscle <BenjaminCM.Boyes@gmail.com>
2024-09-08 13:41:46 -0500

    Fixed some stonecutter recipes.
    changed mod id to bmb for ease of typing.(i'm lazy)

--- src/main/java/net/beefymuscle/metalblocks/BeefsMetalBlocks.java
+++ src/main/java/net/beefymuscle/metalblocks/BeefsMetalBlocks.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BeefsMetalBlocks implements ModInitializer {
-	public static final String MOD_ID = "beefsmetalblock";
+	public static final String MOD_ID = "bmb";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
